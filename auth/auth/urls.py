0fcf8e17dfc860000c4adb0471c1f7c79f890976
dbOg7 <db0g7@outlook.fr>
2022-06-26 14:27:57 +0200

    update

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontapp.urls')),
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
